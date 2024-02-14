19bd1969d78c5a647ff44e0a8ab4d2b22d8c0817
Santolo Toscano <santolo.toscano@xcconsulting.it>
2024-02-14 16:08:35 +0000

    qwfqfw

--- app.js
+++ app.js
@@ -38,10 +38,6 @@ app.post('/journeybuilder/validate/', activity.validate );
 app.post('/journeybuilder/publish/', activity.publish );
 app.post('/journeybuilder/execute/', activity.execute );
 
-$(document).ready(() => {
-  alert(app);
-});
-
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
