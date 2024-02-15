ea797fdadbc0db1d759f292f9f20eecbf1ef97a1
Santolo Toscano <santolo.toscano@xcconsulting.it>
2024-02-15 16:33:34 +0000

    Pass code in js

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -5,3 +5,10 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+//GETENV
+require('dotenv').config({ path: '../../.env' })
+const grant_type = process.env.GRANT_TYPE;
+const client_id = process.env.CLIENT_ID;
+const client_secret = process.env.CLIENT_SECRET;
+const account_id = process.env.ACCOUNT_ID;
+console.log(grant_type, client_id, client_secret, account_id);
