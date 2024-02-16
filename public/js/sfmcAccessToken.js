92f5e7a751364a9cbedb120e5e231cc088d52fc4
Santolo Toscano <santolo.toscano@xcconsulting.it>
2024-02-16 11:33:41 +0000

    Fix axios e Api

--- public/js/sfmcAccessToken.js
+++ public/js/sfmcAccessToken.js
@@ -1,18 +1,31 @@
-const axios = require('axios');
+// Configura require.js con il percorso di Axios
+require.config({
+  paths: {
+    axios: '../../node_modules/axios/dist/axios.min'
+  }
+});
 
-const apiUrl = 'https://mcslcv42qhfcyrj6snc3y3zvm-c4.auth.marketingcloudapis.com/v2/token';
+// Ora puoi utilizzare Axios come dipendenza
+require(['axios'], function (axios) {
+  // URL per ottenere il token
+  const apiUrl = 'https://mcslcv42qhfcyrj6snc3y3zvm-c4.auth.marketingcloudapis.com/v2/token';
 
-const datiDaInviare = {
-  grant_type: "client_credentials",
-  client_id: "fxw952jq0v31b5syhra79vv5",
-  client_secret: "TWxYWlBzb9lsInroD6cJYNYt",
-  account_id: "523021578"
-};
+  // Dati da inviare nella richiesta POST
+  const datiDaInviare = {
+    grant_type: "client_credentials",
+    client_id: "fxw952jq0v31b5syhra79vv5",
+    client_secret: "TWxYWlBzb9lsInroD6cJYNYt",
+    account_id: "523021578"
+  };
 
-axios.post(apiUrl, datiDaInviare)
-  .then(response => {
-    console.log('Risposta:', response.data);
-  })
-  .catch(error => {
-    console.error('Errore nella richiesta POST:', error);
-  });
+  // Esegui la richiesta POST con Axios
+  axios.post(apiUrl, datiDaInviare)
+    .then(response => {
+      // Gestisci la risposta qui
+      console.log('Risposta:', response.data);
+    })
+    .catch(error => {
+      // Gestisci gli errori qui
+      console.error('Errore nella richiesta POST:', error);
+    });
+});
