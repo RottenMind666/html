7c86f24d8947cee8bdde799dfcefd945f703451b
Santolo Toscano <santolo.toscano@xcconsulting.it>
2024-02-16 15:08:41 +0000

    Add express, node-fetch, cors to public

--- public/js/sfmcAccessToken.js
+++ public/js/sfmcAccessToken.js
@@ -1,31 +1,35 @@
-// Configura require.js con il percorso di Axios
 require.config({
   paths: {
-    axios: '../vendor/axios/dist/axios.min'
+    nodefetch: '../vendor/node-fetch/src/index'
   }
 });
 
-// Ora puoi utilizzare Axios come dipendenza
-require(['axios'], function (axios) {
-  // URL per ottenere il token
-  const apiUrl = 'https://mcslcv42qhfcyrj6snc3y3zvm-c4.auth.marketingcloudapis.com/v2/token';
+// Definisci l'URL della tua API
+const apiUrl = 'https://mcslcv42qhfcyrj6snc3y3zvm-c4.auth.marketingcloudapis.com/v2/token';
 
-  // Dati da inviare nella richiesta POST
-  const datiDaInviare = {
-    grant_type: "client_credentials",
-    client_id: "fxw952jq0v31b5syhra79vv5",
-    client_secret: "TWxYWlBzb9lsInroD6cJYNYt",
-    account_id: "523021578"
-  };
+// Dati da inviare nella richiesta POST
+const postData = {
+  grant_type: "client_credentials",
+  client_id: "fxw952jq0v31b5syhra79vv5",
+  client_secret: "TWxYWlBzb9lsInroD6cJYNYt",
+  account_id: "523021578"
+};
 
-  // Esegui la richiesta POST con Axios
-  axios.post(apiUrl, datiDaInviare)
-    .then(response => {
-      // Gestisci la risposta qui
-      console.log('Risposta:', response.data);
-    })
-    .catch(error => {
-      // Gestisci gli errori qui
-      console.error('Errore nella richiesta POST:', error);
-    });
-});
+// Opzioni della richiesta
+const requestOptions = {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify(postData),
+};
+
+// Effettua la chiamata POST con Node-fetch
+fetch(apiUrl, requestOptions)
+  .then(response => response.json())
+  .then(data => {
+    console.log('Risposta dalla API:', data);
+  })
+  .catch(error => {
+    console.error('Errore durante la chiamata POST:', error);
+  });
